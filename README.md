4a25825db69ea51c926ccd1c0af01abdccb82816
Abderahman <abd.adj04@gmail.com>
2024-02-07 18:21:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,57 @@
-# Getting Started with Create React App
+# 25 + 5 Clock
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a simple 25 + 5 Clock built with React.
 
-## Available Scripts
+## Overview
 
-In the project directory, you can run:
+The 25 + 5 Clock is a web application that allows users to set a countdown timer for 25 minutes (also known as a Pomodoro session) followed by a 5-minute break. It helps users manage their time effectively by following the Pomodoro Technique.
 
-### `npm start`
+## Features
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- Set a countdown timer for a 25-minute work session.
+- Automatically switch to a 5-minute break after the work session ends.
+- User-friendly interface with clickable buttons for setting and controlling the timer.s.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Getting Started
 
-### `npm test`
+1. Clone the repository:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   ```bash
+   git clone https://github.com/whoisabd/25-5-Clock.git
 
-### `npm run build`
+2. Install dependencies:
+   
+    ```bash
+     cd 25-5-clock
+     npm install
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+3. Run the app locally:
+   
+    ```bash
+     npm start
+    
+The app will be accessible at http://localhost:3000.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## Deployment:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+  The app is deployed using GitHub Pages. To deploy the app, run:
+  
+     npm run deploy
 
-### `npm run eject`
+Visit https://whoisabd.github.io/25-5-Clock to access the deployed app.
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Technologies Used:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+  - React
+  - JavaScript (ES6+)
+  - HTML5
+  - CSS3
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Credits:
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+  - Myself
+  - This project was bootstrapped with Create React App.
 
-## Learn More
+## License
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This project is licensed under the MIT License - see the [LICENSE](LICENSE.md) file for details.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,223 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import plusImg from './Plus.svg';
+import minusImg from './Minus.svg';
+import playImg from './Play.svg';
+import pauseImg from './Pause icon.svg';
+import resetImg from './Reset.svg';
+import beepAudio from './Beep Buzzer Fail.wav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength : 5,
+      sessionLength: 25,
+      timeLeft: '25',
+      seconds: '00',
+      status: 'Session',
+      timerIsOn : false,
+      currentImg: playImg
+    }
+  }
+
+  handleReset = () => {
+    this.setState({
+      breakLength : 5,
+      sessionLength: 25,
+      timeLeft: '25',
+      seconds: '00',
+      status: 'Session',
+      timerIsOn : false,
+      currentImg: playImg
+    })
+    clearInterval(this.intervalId); 
+    this.intervalId = null;
+    document.getElementById('beep').pause();
+    document.getElementById('beep').load();
+  }
+
+  handleIncrementOrDecrement = (event) => {
+    this.setState((prevState) => {
+      const { breakLength, sessionLength, status, timerIsOn } = prevState;
+      if (timerIsOn) {
+        return prevState
+      }
+      switch(event.target.id) {
+       case 'break-decrement':
+        if(breakLength === 1) {
+          return prevState;
+        }
+        if (status === 'Break') {
+          return {
+            breakLength: breakLength - 1,
+            timeLeft: breakLength - 1 < 10 ? '0' + String(breakLength - 1 ) :String(breakLength - 1 ) ,
+            seconds: '00',
+          };
+        }
+        return {
+          breakLength: breakLength - 1,
+        };
+       case 'break-increment':
+        if(breakLength === 60) {
+          return prevState;
+        }
+        if (status === 'Break') {
+          return {
+            breakLength: breakLength + 1,
+            timeLeft: breakLength + 1 < 10 ? '0' + String(breakLength + 1 ) :String(breakLength + 1 ),
+            seconds: '00'
+          };
+        }
+        return {
+          breakLength: breakLength + 1
+        };
+       case 'session-decrement':
+        if(sessionLength === 1) {
+          return prevState;
+        }
+        if (status === 'Session') {
+          return {
+            sessionLength: sessionLength - 1,
+            timeLeft: sessionLength - 1 < 10 ? '0' + String(sessionLength - 1 ) :String(sessionLength - 1 ),
+            seconds: '00'
+          };
+        }
+        return {
+          sessionLength: sessionLength - 1
+        };
+       case 'session-increment':
+        if(sessionLength === 60) {
+          return prevState;
+        }
+        if (status === 'Session') {
+          return {
+            sessionLength: sessionLength + 1,
+            timeLeft: sessionLength + 1 < 10 ? '0' + String(sessionLength + 1 ) :String(sessionLength + 1 ),
+            seconds: '00'
+          };
+        }
+        return {
+          sessionLength: sessionLength + 1
+        };
+       default:
+        return prevState;
+      }
+    })
+  }
+
+  handlePlayOrPause = () => {
+  this.setState((prevState) => {
+    let { timerIsOn, currentImg } = prevState;
+    timerIsOn = !timerIsOn;
+
+    if (currentImg === playImg) {
+      currentImg = pauseImg;
+    } else {
+      currentImg = playImg;
+    }
+
+    if (!timerIsOn) {
+      clearInterval(this.intervalId); 
+      this.intervalId = null;
+    } else if (!this.intervalId) {
+      this.intervalId = setInterval(this.updateTimer, 1000); 
+    }
+
+    return {
+      timerIsOn: timerIsOn,
+      currentImg: currentImg,
+    };
+  });
+};
+
+intervalId = null; 
+
+updateTimer = () => {
+  this.setState((prevState) => {
+    let { breakLength, sessionLength, timeLeft, seconds, status } = prevState;
+
+    if (Number(seconds) === 0) {
+      if (Number(timeLeft) === 0 && status === 'Session') {
+        document.getElementById('beep').play();
+        return {
+          timeLeft: breakLength < 10 ? '0' + String(breakLength) : String(breakLength),
+          seconds: seconds,
+          status: 'Break'
+        };
+      } else if (Number(timeLeft) === 0 && status === 'Break') {
+        document.getElementById('beep').play();
+         return {
+            timeLeft: sessionLength < 10 ? '0' + String(sessionLength) : String(sessionLength),
+            seconds: seconds,
+            status: 'Session'
+          };
+      } else {
+          return {
+            timeLeft: Number(timeLeft) -1 < 10 ? '0' + String(Number(timeLeft) - 1) : String(Number(timeLeft) - 1),
+            seconds: '59',
+            status: status
+         };
+      }
+    }
+
+
+    seconds = Number(seconds) -1 < 10 ? '0' + String(Number(seconds) - 1) : String(Number(seconds) - 1);
+
+    return {
+      timeLeft: timeLeft,
+      seconds: seconds,
+      status: status,
+    };
+  });
+};
+
+
+  render() {
+    
+    return (
+      <div className='clock-wrapper'>
+        <div className='break-wrapper'>
+          <div id="break-label">Break Length</div>
+          <div className='bottom-wrapper'>
+            <button id="break-decrement" onClick={this.handleIncrementOrDecrement}>
+              <img src={minusImg} alt='decrement'></img>
+            </button>
+            <div id="break-length">{this.state.breakLength}</div>
+            <button id="break-increment" onClick={this.handleIncrementOrDecrement}>
+              <img src={plusImg} alt='increment'></img>
+            </button>
+          </div>
+        </div>
+        <div className='session-wrapper'>
+          <div id="session-label">Session Length</div>
+          <div className='bottom-wrapper'>
+            <button id="session-decrement" onClick={this.handleIncrementOrDecrement}>
+              <img src={minusImg} alt='decrement'></img>
+            </button>
+            <div id="session-length">{this.state.sessionLength}</div>
+            <button id="session-increment" onClick={this.handleIncrementOrDecrement}>
+              <img src={plusImg} alt='increment'></img>
+            </button>
+          </div>
+        </div>
+        <div className='timer-wrapper'>
+          <div id="timer-label">Time Remaining for the current {this.state.status} :</div>
+          <div id="time-left"> {this.state.timeLeft}:{this.state.seconds} </div>
+          <div className='button-group'>
+            <button id="start_stop" onClick={this.handlePlayOrPause}>
+              <img src={this.state.currentImg} alt='play/pause'></img>
+            </button>
+            <button id="reset" onClick={this.handleReset}>
+              <img src={resetImg} alt='reset'></img>
+            </button>
+          </div>
+        </div>
+        <audio src={beepAudio} id='beep'></audio>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Clock;
